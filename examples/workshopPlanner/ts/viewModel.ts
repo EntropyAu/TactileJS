ae6330ccc568099b60514a402f3d95ac0796d77a
Brad Ward <brad@groupmap.com>
2015-08-16 14:00:57 +0800

    no message

--- examples/workshopPlanner/ts/viewModel.ts
+++ examples/workshopPlanner/ts/viewModel.ts
@@ -1,9 +1,62 @@
 module WorkshopPlanner {
+
+  interface Template {
+    name:string;
+    shortDescription:string;
+    tags:string[];
+    duration:string;
+  }
+
+  interface Activity {
+    name:string;
+    shortDescription:string;
+    duration:string;
+  }
+
+  interface Column {
+    name: string;
+    activities: Activity[];
+  }
+
   export class ViewModel {
-    foo: KnockoutObservable<String>;
+    query: KnockoutObservable<string> = ko.observable('');
+    templates: KnockoutObservableArray<Template> = ko.observableArray([]);
+    selectedTag: KnockoutObservable<string> = ko.observable(null);
+
+    filteredTemplates: KnockoutComputed<Template[]>;
+    tags: KnockoutComputed<string[]>;
+
+    columns: KnockoutObservableArray<Column> = ko.observableArray([])
 
     constructor() {
-      this.foo = ko.observable('bar');
+      window['viewModel'] = this;
+      this.filteredTemplates = <KnockoutComputed<Template[]>>ko.pureComputed(this.search.bind(this));
+      this.tags = <KnockoutComputed<string[]>>ko.pureComputed(function() {
+        let tagHash:any = {};
+        this.templates().forEach((template) => (template.tags || []).forEach((t) => tagHash[t] = true));
+        return Object.keys(tagHash);
+      }.bind(this));
+
+      this.selectedTag.subscribe((nv) => nv !== null && this.query(''));
+      this.query.subscribe((nv) => nv !== '' && this.selectedTag(null));
+
+      this.initialize();
+    }
+
+    private initialize() {
+      let self = this;
+      function onSuccess(data:any) {
+        self.templates(<Template[]>jsyaml.load(data).templates);
+      }
+      $.ajax('./workshopPlanner/templates.yaml', { dataType:"text", success: onSuccess });
+    }
+
+    private search():Template[] {
+      if (this.selectedTag()) {
+        return this.templates().filter((t) => t.tags && t.tags.indexOf(this.selectedTag()) !== -1);
+      } else {
+        return this.templates().filter((t) => t.name && t.name.toLowerCase().indexOf(this.query().toLowerCase()) !== -1);
+      }
     }
   }
 }
--- examples/workshopPlanner/workshopPlanner.js
+++ examples/workshopPlanner/workshopPlanner.js
@@ -2,8 +2,38 @@ var WorkshopPlanner;
 (function (WorkshopPlanner) {
     var ViewModel = (function () {
         function ViewModel() {
-            this.foo = ko.observable('bar');
+            var _this = this;
+            this.query = ko.observable('');
+            this.templates = ko.observableArray([]);
+            this.selectedTag = ko.observable(null);
+            this.columns = ko.observableArray([]);
+            window['viewModel'] = this;
+            this.filteredTemplates = ko.pureComputed(this.search.bind(this));
+            this.tags = ko.pureComputed(function () {
+                var tagHash = {};
+                this.templates().forEach(function (template) { return (template.tags || []).forEach(function (t) { return tagHash[t] = true; }); });
+                return Object.keys(tagHash);
+            }.bind(this));
+            this.selectedTag.subscribe(function (nv) { return nv !== null && _this.query(''); });
+            this.query.subscribe(function (nv) { return nv !== '' && _this.selectedTag(null); });
+            this.initialize();
         }
+        ViewModel.prototype.initialize = function () {
+            var self = this;
+            function onSuccess(data) {
+                self.templates(jsyaml.load(data).templates);
+            }
+            $.ajax('./workshopPlanner/templates.yaml', { dataType: "text", success: onSuccess });
+        };
+        ViewModel.prototype.search = function () {
+            var _this = this;
+            if (this.selectedTag()) {
+                return this.templates().filter(function (t) { return t.tags && t.tags.indexOf(_this.selectedTag()) !== -1; });
+            }
+            else {
+                return this.templates().filter(function (t) { return t.name && t.name.toLowerCase().indexOf(_this.query().toLowerCase()) !== -1; });
+            }
+        };
         return ViewModel;
     })();
     WorkshopPlanner.ViewModel = ViewModel;
